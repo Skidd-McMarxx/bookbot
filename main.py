be9119c475ac30db7594d0b392b41c1e98c02aab
Skidd-McMarxx <14020610+Skidd-McMarxx@users.noreply.github.com>
2025-01-14 14:48:10 +0000

    added count characters

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def bookreport(book_path:str):
         file_contents = f.read()
 
         print(count_words(file_contents))
+        print(count_characters(file_contents))
 
 
 def count_words(s:str) -> int:
     return len(s.split())
+
+
+def count_characters(s:str):
+    char_count = {}
+    for char in s.lower():
+        if char not in char_count:
+            char_count[char] = 0
+        char_count[char] += 1
+    return char_count
 
 
 if __name__ == "__main__":
